91019d31ece27a6a11fdcf5edf22f0d98ffc4d2d
Josh LeBlanc <joshcapitals07@gmail.com>
2025-05-01 11:24:55 +0200

    everything but revision working

--- agent.py
+++ agent.py
@@ -6,36 +6,54 @@ from logic import *
 
 class Agent:
     kb_num = 0
-    KB_strs = []
+    KB = []
     symbols = {}
 
     def add_command(self):
         inp = input("Formula to add to knowledge base: ")
-        self.KB_strs.append((inp, self.kb_num))
+        self.KB.append((inp, self.kb_num))
         self.kb_num += 1
+        self.print_belief_base()
 
     def print_belief_base(self):
         print("--------------------------------------")
         print("Belief base:")
-        if not self.KB_strs:
+        if not self.KB:
             print("EMPTY")
         else:
-            for b in self.KB_strs:
+            for b in self.KB:
                 print(str(b[1]) + ". " + b[0])
         print("--------------------------------------")
 
     def clear_command(self):
         self.symbols = {}
-        self.KB_strs = []
+        self.KB = []
         self.kb_num = 0
+        self.print_belief_base()
+
+    def delete_command(self):
+        inp = input("Formula to delete from knowledge base: ")
+        for belief, i in self.KB:
+            if inp == belief:
+                self.KB.remove((belief, i))
+        self.print_belief_base()
+
+    def revise_command(self):
+        inp = input("Formula to resolve with the knowledge base: ")
+        for belief, i in self.KB:
+            if resolution([belief], "~(" + inp.strip() + ")"):
+                self.KB.remove((belief, i))
+        self.KB.append((inp, self.kb_num))
+        self.kb_num += 1
+        self.print_belief_base()
 
     def entails_command(self):
         inp = input("Fomula to check for entailment: ")
-        print("entails: " + str(resolution(self.KB_strs, inp)))
+        print("entails: " + str(resolution(self.KB, inp)))
 
     # boolean is false if command is 'q' and the agent should be quit, true otherwise
     def process_input(self):
-        valid_commands = ['a', 'b', 'e', 'c', 'q']
+        valid_commands = ['a', 'b', 'd', 'r', 'e', 'c', 'q']
         command = input("Input command: ")
         print("""======================================
 COMMAND RESULT:""")
@@ -47,8 +65,12 @@ COMMAND RESULT:""")
             self.print_belief_base()
         elif command == 'c':
             self.clear_command()
+        elif command == 'd':
+            self.delete_command()
         elif command == 'e':
             self.entails_command()
+        elif command == 'r':
+            self.revise_command()
         elif command == 'q':
             return False
         print("======================================")
@@ -63,11 +85,13 @@ COMMAND RESULT:""")
 belief revision agent
 --------------------------------------
 commands:
-- `a`: add a statement to the belief base
+- `a`: add a statement to the belief base (expansion)
 - `b`: print belief base
 - `c`: clear the belief base
+- `d`: delete a statement from the belief base (contraction)
 - `e`: enter a statement to check if it is entailed by the belief base
 - `q`: quit the agent
+- `r`: revise the belief base with the following statement (revision)
 --------------------------------------
                   """)
 
