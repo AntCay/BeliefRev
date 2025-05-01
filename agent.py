0368497038d745c0adde760885b8df402ba00c1f
Josh LeBlanc <joshcapitals07@gmail.com>
2025-05-01 15:15:11 +0200

    revision and contraction working

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ from sympy.parsing.sympy_parser import parse_expr
 from sympy.logic.boolalg import And, Not, Implies
 from sympy.logic.inference import satisfiable
 from logic import *
+from revision import *
 
 class Agent:
     kb_num = 0
@@ -11,8 +12,10 @@ class Agent:
 
     def add_command(self):
         inp = input("Formula to add to knowledge base: ")
-        self.KB_strs.append((inp, self.kb_num))
-        self.kb_num += 1
+        expansion(self, inp)
+        self.print_belief_base()
+        # self.KB_strs.append((inp, self.kb_num))
+        # self.kb_num += 1
 
     def print_belief_base(self):
         print("--------------------------------------")
@@ -24,6 +27,16 @@ class Agent:
                 print(str(b[1]) + ". " + b[0])
         print("--------------------------------------")
 
+    def delete_command(self):
+        inp = input("Enter belief to contract from belief base: ")
+        delete_belief(self, inp)
+        self.print_belief_base()
+
+    def revise_command(self):
+        inp = input("Enter belief to revise with belief base: ")
+        revise(self, inp)
+        self.print_belief_base()
+
     def clear_command(self):
         self.symbols = {}
         self.KB_strs = []
@@ -31,14 +44,14 @@ class Agent:
 
     def entails_command(self):
         inp = input("Fomula to check for entailment: ")
-        print("entails: " + str(resolution(self.KB_strs, inp)))
+        print("Entails: " + str(entailment(self.KB_strs, inp)))
 
     # boolean is false if command is 'q' and the agent should be quit, true otherwise
     def process_input(self):
-        valid_commands = ['a', 'b', 'e', 'c', 'q']
+        valid_commands = ['a', 'b', 'd', 'r', 'e', 'c', 'q']
         command = input("Input command: ")
         print("""======================================
-COMMAND RESULT:""")
+              COMMAND RESULT:""")
         if command not in valid_commands:
             print("INVALID COMMAND")
         if command == 'a':
@@ -47,12 +60,17 @@ COMMAND RESULT:""")
             self.print_belief_base()
         elif command == 'c':
             self.clear_command()
+        elif command == 'd':
+            self.delete_command()
         elif command == 'e':
             self.entails_command()
+        elif command == 'r':
+            self.revise_command()
         elif command == 'q':
             return False
         print("======================================")
         return True
+
 
     def main(self):
         # agent loop
@@ -63,11 +81,13 @@ COMMAND RESULT:""")
 belief revision agent
 --------------------------------------
 commands:
-- `a`: add a statement to the belief base
+- `a`: add a statement to the belief base (expansion)
 - `b`: print belief base
 - `c`: clear the belief base
+- `d`: delete a statement from the belief base (contraction)
 - `e`: enter a statement to check if it is entailed by the belief base
 - `q`: quit the agent
+- `r`: revise the belief base with the following statement (revision)
 --------------------------------------
                   """)
 
--- revision.py
+++ revision.py
@@ -1,35 +1,64 @@
 from  logic import *
 import copy
+from itertools import combinations
 
 def contract(agent, new_belief):
     contracted_belief = copy.copy(agent.KB_strs)
+    if check_consistency(contracted_belief, new_belief):
+        return contracted_belief
+    else:
+        for i in range(len(contracted_belief) - 1, 0, -1):
+            combs = list(combinations(contracted_belief, i))
+            combs.reverse()
+            for comb in combs:
+                if check_consistency(comb, new_belief):
+                    agent.KB_strs = list(comb)
+                    return comb
+
+    agent.KB_strs = []
+    return []
+            
+    # for i in range(len(agent.KB_strs)):
+    #     if not check_consistency([agent.KB_strs[i]], new_belief):
+    #         contracted_belief.remove(agent.KB_strs[i])
+    # agent.KB_strs = contracted_belief
+
+def delete_belief(agent, new_belief):
+    contracted_belief = copy.copy(agent.KB_strs)
     for i in range(len(agent.KB_strs)):
-        if not check_consistency([agent.KB_strs[i]], new_belief):
+        if agent.KB_strs[i][0] == new_belief:
             contracted_belief.remove(agent.KB_strs[i])
     agent.KB_strs = contracted_belief
     return contracted_belief
 
 def expansion(agent, new_belief):
     expanded_belief = copy.copy(agent.KB_strs)
-    if not new_belief in agent.KB_strs[0]:
-        expanded_belief.append((new_belief, agent.kb_num))
-        agent.KB_strs = expanded_belief
-        agent.kb_num += 1
+
+    for belief, _ in expanded_belief:
+        if new_belief == belief:
+            return expanded_belief
+
+    expanded_belief.append((new_belief, agent.kb_num))
+    agent.kb_num += 1
+    agent.KB_strs = expanded_belief
     return expanded_belief
 
 def revise(agent, new_belief):
     # Check if the new belief is already entailed by the belief base
-    if entailment(agent.KB_strs, new_belief):
-        return agent.KB_strs  # No changes needed
-
-    # If the new belief is inconsistent, contract and expand
-    if not check_consistency(agent.KB_strs, new_belief):
-        contract(agent, new_belief)
-        expansion(agent, new_belief)
-        return agent.KB_strs
-
-    # If the new belief is not entailed and consistent, expand the belief base
-    formulas = [i[0] for i in agent.KB_strs]
-    if not new_belief == "" and not new_belief in formulas:
-        expansion(agent, new_belief)
+    # if entailment(agent.KB_strs, new_belief):
+    #     return agent.KB_strs  # No changes needed
+    #
+    # # If the new belief is inconsistent, contract and expand
+    # if not check_consistency(agent.KB_strs, new_belief):
+    #     contract(agent, new_belief)
+    #     expansion(agent, new_belief)
+    #     return agent.KB_strs
+    #
+    # # If the new belief is not entailed and consistent, expand the belief base
+    # formulas = [i[0] for i in agent.KB_strs]
+    # if not new_belief == "" and not new_belief in formulas:
+    #     expansion(agent, new_belief)
+    # return agent.KB_strs
+    contract(agent, new_belief)
+    expansion(agent, new_belief)
     return agent.KB_strs
