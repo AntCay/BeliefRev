11c6960e98c67e45d80db9cce39475999e561362
rifkifi <rifkifi@gmail.com>
2025-05-01 17:50:18 +0200

    testing for revision added

--- testing.py
+++ testing.py
@@ -3,17 +3,18 @@ from sympy.logic.boolalg import simplify_logic
 import copy
 from agent import Agent
 
-def succces(agent, new_belief):
-    # Check if the new belief is already entailed by the belief base
-    if entailment(agent.KB_strs, new_belief):
-        return "The new belief is already entailed by the belief base."
-
-    test_agent = copy.copy(agent)
-    revise(test_agent, new_belief)
+def succces_revision():
+    test_agent = Agent()
+    test_agent.expand_command("p")
+    test_agent.expand_command("p>>q")
+    new_belief = "q"
+    
+    test_agent.revise_command(new_belief)
+    
     for i in test_agent.KB_strs:
         if i[0] == new_belief:
-            return True
-    return False
+            return print("Success revision test: " + str(True))
+    return print("Success revision test: " + str(False))
 
 def success_contraction():
     agent: Agent = Agent()
@@ -25,15 +26,19 @@ def success_contraction():
 
     print("Success contraction test: " + str(not includes_clause_in_closure_of_knowledge_base(agent, "b")))
 
-def inclusion(agent, new_belief):
+def inclusion_revision():
+    agent = Agent()
+    
+    agent.expand_command("p")
+    agent.expand_command("p>>q")
+    new_belief = "q"
+    
     exp_agent = copy.copy(agent)
     rev_agent = copy.copy(agent)
-    exp_belief = expansion(exp_agent, new_belief)
-    rev_belief = revise(rev_agent, new_belief)
+    exp_agent.expand_command(new_belief)
+    rev_agent.revise_command(new_belief)
 
-    if all(elem in exp_belief for elem in rev_belief):
-        return True
-    return False
+    print("Inclusion revision test: " + str(all(elem in exp_agent.KB_strs for elem in rev_agent.KB_strs)))
 
 def inclusion_contraction():
     agent: Agent = Agent()
@@ -50,22 +55,25 @@ def inclusion_contraction():
 
     print("Inclusion contraction test: " + str(after_kb.issubset(before_kb)))
 
-def vacuity(agent, new_belief):
-    # Check if the new belief is already entailed by the belief base
-    if entailment(agent.KB_strs, new_belief):
-        return "The new belief is already entailed by the belief base."
-
+def vacuity_revision():
+    agent = Agent()
+    
+    agent.expand_command("p")
+    agent.expand_command("p>>q")
+    new_belief = "q"
+    
     # extract formulas in belief base
     formulas = [i[0] for i in agent.KB_strs]
+    
     # check if the negated new belief is already in the belief base
     if not str(Not(new_belief)) in formulas:
         exp_agent = copy.copy(agent)
         rev_agent = copy.copy(agent)
-        exp_belief = expansion(exp_agent, new_belief)
-        rev_belief = revise(rev_agent, new_belief)
-        return exp_belief == rev_belief
+        exp_agent.expand_command(new_belief)
+        rev_agent.revise_command(new_belief)
+        return print("Vacuity Revision test : " + str(exp_agent.KB_strs == rev_agent.KB_strs))
     else:
-        return "The negated new belief is already in the knowledge base."
+        return print("Vacuity Revision test: The negated new belief is already in the knowledge base.")
 
 def vacuity_contraction():
     agent: Agent = Agent()
@@ -83,25 +91,33 @@ def vacuity_contraction():
     print("Vacuity contraction test: " + str(before_kb == after_kb))
 
 
-def consistency(agent, new_belief):
+def consistency_revision():
+    agent = Agent()
+    
+    agent.expand_command("p")
+    agent.expand_command("p>>q")
+    new_belief = "q"
+    
     # check consistency of the new belief and the belief base
     if check_consistency(new_formula=new_belief):
-        return check_consistency(agent.KB_strs)
-    return False
+        print("Consistency revision test: "  + str(check_consistency(agent.KB_strs)))
 
-def extentionality(agent, new_belif_a, new_belif_b):
-    # Parse the string formulas into sympy expressions
-    formula_a = simplify_logic(new_belif_a)
-    formula_b = simplify_logic(new_belif_b)
-    # Check equivalence
-    if formula_a == formula_b:
-        a_agent = copy.copy(agent)
-        b_agent = copy.copy(agent)
-        a_rev = revise(a_agent, new_belif_a)
-        b_rev = revise(b_agent, new_belif_b)
-        return a_rev == b_rev
-    else: 
-        return "The two formulas are not equivalent."
+
+def extentionality_revision():
+    agent = Agent()
+    
+    agent.expand_command("a")
+    agent.expand_command("c")
+    agent.expand_command("a>>b")
+    agent.expand_command("a<>c")
+    new_belief_a = "a"
+    new_belief_b = "c"
+    
+    a_agent = copy.copy(agent)
+    b_agent = copy.copy(agent)
+    a_agent.revise_command(new_belief_a)
+    b_agent.revise_command(new_belief_b)
+    print("Extentionality revision test: " + str(a_agent.KB_strs == b_agent.KB_strs))
 
 def extentionality_contraction():
     agentA: Agent = Agent()
@@ -133,5 +149,10 @@ if __name__ == "__main__":
     inclusion_contraction()
     vacuity_contraction()
     extentionality_contraction()
+    succces_revision()
+    inclusion_revision()
+    vacuity_revision()
+    consistency_revision()
+    extentionality_revision()
 
     
